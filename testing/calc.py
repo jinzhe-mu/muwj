d904bb18e794d88d6e85c66b622a2d8f3401121c
muwj <1421827450@qq.com>
2020-05-16 00:04:53 +0800

    pytest

--- testing/calc.py
+++ testing/calc.py
@@ -1,11 +1,11 @@
-from decimal import Decimal
+from decimal import Decimal  # 导入Decimal
 
 
-class Calc:
+class Calc:  # 定义类
 
-    def add(self, a, b):
-        return float(Decimal(str(a)) + Decimal(str(b)))
+    def add(self, a, b):  # 定义方法，传入a，b两个数值
+        return float(Decimal(str(a)) + Decimal(str(b)))  # 计算a+b的值并返回，强制转化十进制的浮点型数据
 
-    def div(self, a, b):
-        return a / b
+    def div(self, a, b):  # 定义方法，传入a，b两个数值
+        return a / b  # 计算a/b的值并返回
 
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,31 +1,31 @@
-import pytest
-import yaml
+import pytest  # 导入pytest包
+import yaml  # 导入yaml包
 
 
-from testing.calc import Calc
+from testing.calc import Calc  # 导入testing包下的calc.py文件中的Calc类
 
 
 
-class TestCalc():
-    def setup_class(sefl):
-        sefl.calc = Calc()
+class TestCalc():  # 定义测试类
+    def setup_class(sefl):  # 定义steup_class 方法（类级别）
+        sefl.calc = Calc()  # 实例化Calc类
         print("setup_class")
 
-    def teardown_class(self):
+    def teardown_class(self):  # 定义teardown_class 方法（类级别）
         print("teardown_class")
 
-    @pytest.mark.parametrize('a, b, expect', yaml.safe_load(open("data.yaml")))
-    def test_add_1(self, a, b, expect):
-        result = self.calc.add(a, b)
-        print(f"result={a}+{b}={result}")
-        assert result == expect
+    @pytest.mark.parametrize('a, b, expect', yaml.safe_load(open("data.yaml")))  # 从data.yaml文件中传入列表值赋值给a, b, expect，供test_add_1方法调用
+    def test_add_1(self, a, b, expect):  # 定义测试方法，传入a，b，expect数值
+        result = self.calc.add(a, b)  # 调用calc.add方法进行计算并赋值给result
+        print(f"result={a}+{b}={result}")  # 打印内容
+        assert result == expect  # 断言result 等于 expect
 
-    @pytest.mark.parametrize('a, b, expect', yaml.safe_load(open("data1.yaml")))
-    def test_div(self, a, b, expect):
-        result = self.calc.div(a, b)
-        print(f"result={a}/{b}={result}")
-        assert result == expect
+    @pytest.mark.parametrize('a, b, expect', yaml.safe_load(open("data1.yaml")))  # 从data1.yaml文件中传入列表值赋值给a, b, expect，供test_div方法调用
+    def test_div(self, a, b, expect):  # 定义测试方法，传入a，b，expect数值
+        result = self.calc.div(a, b)  # 调用calc.add方法进行计算并赋值给result
+        print(f"result={a}/{b}={result}")  # 打印内容
+        assert result == expect  # 断言result 等于 expect
 
 
-if __name__ == '__main__':
-    pytest.main(['-v', '-s', 'TestCalc'])
+if __name__ == '__main__':  # 定义main函数
+    pytest.main(['-v', '-s', 'TestCalc'])   # 运行当前测试文件
